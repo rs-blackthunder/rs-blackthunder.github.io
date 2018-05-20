0066966c3ae15a7ae41d1edda86c9a7c30d31424
Matthew Grove <39408354+mgrove36@users.noreply.github.com>
2018-05-20 19:40:06 +0100

    Update privacy-policy.js

--- assets/privacy-policy.js
+++ assets/privacy-policy.js
@@ -1,5 +1,9 @@
 $(document).ready ( function(){
 	if(Cookies.get("blackThunderFLLCookiePolicy__Accepted") == "true") {
 		$(".button").show();
+		$(".button").animate({
+			opacity: 0
+		}, 500);
+		$(".button").hide();
 	}
-});
+});​
