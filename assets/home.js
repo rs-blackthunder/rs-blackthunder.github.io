ed970d1426ce9069ee695d58f597dd9ee86b10c7
Matthew Grove <39408354+mgrove36@users.noreply.github.com>
2018-08-22 15:47:39 +0100

    Revert "Make editing main content more efficient"
    This reverts commit a2710d6f132bb6e14806601ebfbd87eab9ca3a7d.

--- assets/home.js
+++ assets/home.js
@@ -8,6 +8,7 @@ var aTextText =destinationText.innerHTML.split("<br>");
 // TITLE
 
 // set up text to print, each item in array is new line
+<<<<<<< HEAD
 // var aText = new Array(
 // "Welcome"
 // );
@@ -15,11 +16,16 @@ var aTextText =destinationText.innerHTML.split("<br>");
 =======
 var aText = document.getElementById("typedTitleContent").innerHTML.split("<br>");
 >>>>>>> parent of bb61ca1... Attempt fix to text not typing
+=======
+var aText = new Array(
+"Welcome"
+);
+>>>>>>> parent of a2710d6... Make editing main content more efficient
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
-
+ 
 var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
@@ -28,7 +34,12 @@ function typewriterTitle()
 {
 	sContents =  ' ';
 	iRow = Math.max(0, iIndex-iScrollAt);
+<<<<<<< HEAD
 
+=======
+	var destination = document.getElementById("typedTitle");
+ 
+>>>>>>> parent of a2710d6... Make editing main content more efficient
 	while ( iRow < iIndex ) {
 		sContents += aText[iRow++] + '<br>';
 	}
@@ -54,6 +65,7 @@ function typewriterTitle()
 // MAIN TEXT
 
 // set up text to print, each item in array is new line
+<<<<<<< HEAD
 // var aTextText = new Array(
 // "We are Black Thunder: a FIRST LEGO League team from Reading School, UK. We are a group of around 40 11-16 year olds who enjoy robotics, and meet each Friday after school, to work on robot-related tasks; one of them is the FLL.",
 // "",
@@ -63,6 +75,13 @@ function typewriterTitle()
 =======
 var aTextText = document.getElementById("typedTextContent").innerHTML.split("<br>");
 >>>>>>> parent of bb61ca1... Attempt fix to text not typing
+=======
+var aTextText = new Array(
+"We are Black Thunder: a FIRST LEGO League team from Reading School, UK. We are a group of around 40 11-16 year olds who enjoy robotics, and meet each Friday after school, to work on robot-related tasks; one of them is the FLL.",
+"",
+"We have a blog, YouTube channel and Twitter feed; our YouTube channel and Twitter feed can be seen below."
+);
+>>>>>>> parent of a2710d6... Make editing main content more efficient
 var iSpeedText = 30; // time delay of print out
 var iIndexText = 0; // start printing array at this position
 var iArrLengthText = aTextText[0].length; // the length of the text array
@@ -76,7 +95,12 @@ function typewriterText()
 {
 	sContentsText =  ' ';
 	iRowText = Math.max(0, iIndexText-iScrollAtText);
+<<<<<<< HEAD
 
+=======
+	var destinationText = document.getElementById("typedText");
+ 
+>>>>>>> parent of a2710d6... Make editing main content more efficient
 	while ( iRowText < iIndexText ) {
 		sContentsText += aTextText[iRowText++] + '<br>';
 	}
